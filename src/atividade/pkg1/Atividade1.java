a297b6e6208f6ed2b9222e5de415eb1c99989557
yokaithefato <wallacewwc2015wwk@gmail.com>
2024-06-21 22:54:47 -0300

    add code

---
+++ src/atividade/pkg1/Atividade1.java
@@ -0,0 +1,22 @@
+
+package atividade.pkg1;
+
+
+public class Atividade1 {
+
+   
+    public static void main(String[] args) {
+        System.out.println("exibindo numeros impares de 0 a 20 ");
+        
+        
+        for(int i=0; i<=20;i++){
+            if(i%2==0){
+                
+            }
+            else{
+                System.out.println("numero " +i+" e impar");
+            }
+        }
+    }
+    
+}
